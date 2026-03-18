1a95fda43590f621ea95270becdf5358d350897f
bhupes <bhupesh.aawasthi952@gmail.com>
2026-03-18 22:18:42 +0100

    fixing bug

--- models/marts/Aggregated_fact/fct_gender_monthly_health_distribution.sql
+++ models/marts/Aggregated_fact/fct_gender_monthly_health_distribution.sql
@@ -14,14 +14,12 @@ WITH source AS (
 )
 
 select
-    gender,
-    program_month,
-    ROUND(100 * COUNT(CASE WHEN bmi_category = 'Obese Class II' THEN 1 END) / COUNT(*), 1) AS obese_ii_percentage,
-    ROUND(100 * COUNT(CASE WHEN bmi_category = 'Obese Class I' THEN 1 END) / COUNT(*), 1) AS obese_i_percentage,
-    ROUND(100 * COUNT(CASE WHEN bmi_category = 'Overweight' THEN 1 END) / COUNT(*), 1) AS overweight_percentage,
-    ROUND(100 * COUNT(CASE WHEN bmi_category = 'Healthy Range' THEN 1 END) / COUNT(*), 1) AS healthy_percentage,
-    ROUND(100 * COUNT(CASE WHEN bmi_category = 'underweight' THEN 1 END) / COUNT(*), 1) AS underweight_percentage,
-    COUNT(*) as total_sample_size
-from source
+    gender,program_month,
+    round(100*COUNT(CASE WHEN bmi_category = 'Obese Class II' THEN 1 END) / COUNT(*), 1) AS obese_ii_percentage,
+    round(100*COUNT(CASE WHEN bmi_category = 'Obese Class I' THEN 1 END) / COUNT(*), 1) AS obese_i_percentage,
+    round(100*COUNT(CASE WHEN bmi_category = 'Overweight' THEN 1 END) / COUNT(*), 1) AS overweight_percentage,
+    round(100*COUNT(CASE WHEN bmi_category = 'Healthy Range' THEN 1 END) / COUNT(*), 1) AS healthy_percentage,
+    round(100*COUNT(CASE WHEN bmi_category = 'underweight' THEN 1 END) / COUNT(*), 1) AS underweight_percentage,
+    count(*) as total_sample_size from source
 group by  1, 2
 order by  2, 1
